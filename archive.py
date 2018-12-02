fc25a415a67c80fda5861f8d2245ae6098000015
Emily Hu <emilyhu1224@gmail.com>
2018-12-02 02:21:07 -0500

    cleanups

--- archive.py
+++ archive.py
@@ -6,15 +6,3 @@ def sortSamplesIntoTiers(samples, cutoffs):
 
 	return tiers
 
-# Get 20,000 random businesses (without duplicates)
-# SAMPLE_SIZE = 20000
-# randomBusinesses  = random.sample(businesses, SAMPLE_SIZE)
-# samples = [simplifiedBusiness(randomBusiness) for randomBusiness in randomBusinesses]
-# Display all
-# for sample in samples:
-# 	sample.display()
-
-# reviewCounts = [sample.reviewCount for sample in samples]
-# stars = [sample.stars for sample in samples]
-
-# print("{}, {}, {}, {}".format(numpy.percentile(reviewCounts, 25), numpy.percentile(reviewCounts, 50), numpy.percentile(reviewCounts, 75), numpy.percentile(reviewCounts, 100)))
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import json
 import numpy
+import random
 import calculations
+
+file = open('data.json')
+businesses = json.load(file)['businesses']
 
 class simplifiedBusiness:
 	# Construct a simplifiedBusiness (obj) from a business (dict)
@@ -31,22 +35,37 @@ def getTier(number, cutoffs):
 			return i
 	return len(cutoffs)
 
-SAMPLE_SIZE = 1000
-CUTOFFS = [100, 500, 800]
+def part1():
+	# Get 20,000 random businesses (without replacement)
+	SAMPLE_SIZE = 20000
+	randomBusinesses  = random.sample(businesses, SAMPLE_SIZE)
+	samples = [simplifiedBusiness(randomBusiness) for randomBusiness in randomBusinesses]
+	# Display all
+	# for sample in samples:
+	# 	sample.display()
 
-file = open('data.json')
-businesses = json.load(file)['businesses']
+	reviewCounts = [sample.reviewCount for sample in samples]
+	stars = [sample.stars for sample in samples]
 
-tiers = [[], [], [], []]
-while min([len(tier) for tier in tiers]) < SAMPLE_SIZE:
-	randomBusiness  = calculations.getRandomSample(businesses)
-	tierIndex = getTier(randomBusiness["review_count"], CUTOFFS)
-	if len(tiers[tierIndex]) < SAMPLE_SIZE:
-		sample = simplifiedBusiness(randomBusiness)
-		if sample not in tiers[tierIndex]:
-			tiers[tierIndex].append(sample)
+	print("{}, {}, {}, {}".format(numpy.percentile(reviewCounts, 25), numpy.percentile(reviewCounts, 50), numpy.percentile(reviewCounts, 75), numpy.percentile(reviewCounts, 100)))
 
-for tier in tiers:
-	average, margin, lower, upper = calculations.getCI([sample.stars for sample in tier])
-	sigma = numpy.std([sample.stars for sample in tier])
-	print("{}, {}+-{}=({},{})".format(sigma, average, margin, lower, upper))
+def part2():
+	# Get 1,000 random businesses for each bucket (without replacement)
+	SAMPLE_SIZE = 1000
+	CUTOFFS = [100, 500, 800]
+
+	tiers = [[], [], [], []]
+	while min([len(tier) for tier in tiers]) < SAMPLE_SIZE:
+		randomBusiness  = calculations.getRandomSample(businesses)
+		tierIndex = getTier(randomBusiness["review_count"], CUTOFFS)
+		if len(tiers[tierIndex]) < SAMPLE_SIZE:
+			sample = simplifiedBusiness(randomBusiness)
+			if sample not in tiers[tierIndex]:
+				tiers[tierIndex].append(sample)
+
+	for tier in tiers:
+		average, margin, lower, upper = calculations.getCI([sample.stars for sample in tier])
+		sigma = numpy.std([sample.stars for sample in tier])
+		print("{}, {}+-{}=({},{})".format(sigma, average, margin, lower, upper))
+
+part2()
